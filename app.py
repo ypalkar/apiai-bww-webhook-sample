ceee03fbe1beb855914187f0abf049fc8c227901
Yashwant Palkar <ypalkar@cisco.com>
2017-03-12 20:01:28 -0700

    Added first version

---
+++ app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "chatbotFoodService":
+        return {}
+    baseurl = "http://oamp-alexa-v2.cloudhub.io/alexa/products?"
+    bww_query = makebwwQuery(req)
+    if bww_query is None:
+        return {}
+    bww_url = baseurl + bww_query
+    result = urlopen(bww_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makebwwQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    product = parameters.get("product")
+    quantity= parameters.get("quantity")
+	type=parameters.get("type")
+	return "productName="+product+"quantityName="+quantity+"productType="+type;
+    #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    listPrice = data.get('listPrice')
+    if query is None:
+        return {}
+
+    speech = "your amount for current order is " + listPrice
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-chatFoodOrder-webhook-master"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
